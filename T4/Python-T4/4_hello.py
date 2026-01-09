6a8926cbdad1d1f64b1e129ef1198805a33e72d8
stackv <vraj18052007@gmail.com>
2026-01-09 08:46:25 +0530

    Delete T4 directory

--- T4/Python-T4/4_hello.py
+++
@@ -1,8 +0,0 @@
-
-import streamlit as st
-
-st.set_page_config(page_title="Streamlitpage3",page_icon="🍰",layout="centered")
-st.title("Number Input Demo")
-age=st.number_input("Enter your Age:",min_value=0,max_value=80,value=25)
-rating=st.slider("Give Rating:",min_value=0,max_value=10,value=7,step=2)
-st.write(age)
--- T4/streamlit4.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.set_page_config(page_title="Forth NUmber",page_icon="🐇",layout="centered")
-st.title("Number Input DEmo")
-age = st.number_input("Enter your age",min_value=10,max_value=80,value=18)
-ratings = st.slider("GIve rating",min_value=0,max_value=10,value=7,step=2)
-st.write(age)
